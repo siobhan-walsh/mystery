94c23d41950f212ff978fd844de8b68198f6c4a8
JessD <jess423s@gmail.com>
2015-11-14 17:47:52 -0800

    ongoing fixing

--- notifications.php
+++ notifications.php
@@ -5,9 +5,9 @@
         <div class = 'content'>
             <h4>Notification</h4>
             
-          
+        <div id="notificatDiv">
         <div>
-        	<img src="img/frdsList/F2.png"/><p>Katie has accepted your friend request! </p><hr/>
+        	<img src="img/frdsList/F1.png"/><p>Katie has accepted your friend request! </p><hr/>
         </div>
         
         <div>
@@ -15,10 +15,10 @@
         </div>
         
     	<div>
-        	<img src="img/frdsList/F2.png"/><p>Katie has accepted your friend request! </p><hr/>
+        	<img src="img/frdsList/F4.png"/><p>Katie has accepted your friend request! </p><hr/>
         </div>
     	<div>
-        	<img src="img/frdsList/F2.png"/><p>Katie has accepted your friend request! </p><hr/>
+        	<img src="img/frdsList/F1.png"/><p>Katie has accepted your friend request! </p><hr/>
         </div>
-       
+        </div>
 <?php include("footer.php"); ?>
