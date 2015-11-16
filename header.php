39bc8d444fcf372d2d2406d4d7b8825688fef808
siobhan-walsh <siobhanemmarose@gmail.com>
2015-11-15 20:25:24 -0800

    user info in header

--- header.php
+++ header.php
@@ -3,7 +3,14 @@
 	<div class = 'tablet-size'>	
     <div class = 'scroll'>
     <div class = 'header'>
-        <br><br>
-        <div><img src = 'img/logo.png'></div>
-    
+
+        	<div class = 'logospot'>
+            	<img id='logo' src = 'img/logo.png'>
+            </div>
+            <div id='userinfo'>
+                <img id='userpic' src='img/frdsList/F1.png'><br>
+                <span id='usern'>name</span>
+        	</div>
+   
+    	
     </div>
