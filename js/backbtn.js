b24ea882705a1756761d1bb0edc7ab46a8cc7163
k8esak <k8esak@gmail.com>
2015-11-15 17:49:47 -0800

    try this again, copy and paste from mamp..NO DRAGGING

---
+++ js/backbtn.js
@@ -0,0 +1,13 @@
+// back button
+
+
+		 	console.log("yo back button script haha");
+		 	var backbtn = document.getElementById('backbtn');
+			
+			backbtn.onclick = function(){
+				
+				
+				window.history.back();
+			};
+		 
+	
