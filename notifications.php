a50da763a18bf005f316a44330dc83586da0b5b4
yingyi1204 <doraemonlyy@gmail.com>
2015-11-20 13:28:37 -0800

    notification page fixed
    copy&paste back from github.

--- notifications.php
+++ notifications.php
@@ -5,8 +5,8 @@
   
 <h2> Notifications</h2>
             
-        <div class = 'noticontent'>
-    
+        <div id = 'noticontent' class = 'noticontent'>
+    <!--
             <div class="notice info"><img id="green" src="img/circle_green.png"/><img src="img/frdsList/F1.png"/><p>Alison accepted your friend request!</p></div>
 
 			<div class="notice success"><img id="green" src="img/circle_green.png"/><img src="img/frdsList/F2.png"/><p>Dylan accepted your request to play Murder in Sin City!</p></div>
@@ -15,7 +15,7 @@
 			
             <div class="notice error"><img id="green" src="img/circle_purple.png"/><img src="img/frdsList/F2.png"/><p>Joshua sent you a message!</p></div>
              <br></br>  <br></br>
-             
+                -->
              </div>
              
         
@@ -63,16 +63,23 @@ $(document).ready(function(){
 
             
 */
+		
+		seen();
+		getrequestinfo();
+		
+		function seen(){
 			$.ajax({
-				url:"server/seerequest-server.php",
+				url:"server/seennotification.php",
 				type:"POST",
 				dataType:"JSON",
 				data:{
-					status:'zero'	
+					status:'seen'	
 				},
-				success:function(seerequest){
+				success:function(seennotification){
 					
-					console.log('seerequest', seerequest);
+					console.log('seennotification', seennotification);
+					
+					
 			
 				},
 				error: function(jqXHR, textStatus, errorThrown) {
@@ -81,6 +88,171 @@ $(document).ready(function(){
 				  
 				}
 				
+			});	
+		};
+		
+		function getrequestinfo(){
+			
+			$.ajax({
+				url:"server/seerequest.php",
+				type:"POST",
+				dataType:"JSON",
+				data:{
+					status:'list'	
+
+				},
+				success:function(seerequest){
+                    
+                    var seerequestSize = objectSize(seerequest);
+					
+					console.log('seerequest', seerequest);
+                    
+				
+					for(var i =0; i < seerequestSize; i++){
+						
+						var div = document.createElement('div');
+						div.class = 'notice info';
+						
+						var limg = document.createElement('img');
+						$(limg).addClass('green');
+						limg.src = 'img/circle_green.png';
+						
+						var img = document.createElement('img');
+						img.src = seerequest[i][0].avatar;
+						
+						var p = document.createElement('p');
+						p.innerHTML = seerequest[i][0].user_name + "sent you a friend request!"
+						
+						var abutton = document.createElement('button');
+						abutton.innerHTML = 'accept';
+						
+						$(abutton).addClass('respbtn');
+						$(abutton).data('frienduserid', seerequest[i][0].user_id);
+						$(abutton).data('status', 'accept');
+						//button.class = 'acceptbtn';
+						
+						var dbutton = document.createElement('button');
+						dbutton.innerHTML = 'deny';
+						$(dbutton).addClass('respbtn');
+						$(dbutton).data('frienduserid', seerequest[i][0].user_id);
+						$(dbutton).data('status', 'deny');
+						var divcase = document.getElementById('noticontent');
+						
+						divcase.appendChild(div);
+						divcase.appendChild(limg);
+						divcase.appendChild(img);
+						divcase.appendChild(p);
+						divcase.appendChild(abutton);
+						divcase.appendChild(dbutton);
+						
+						accept();
+							
+						
+					}	
+
+			
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+						console.log('seerequest error');
+						console.log(jqXHR.statusText, errorThrown, textStatus);
+				  
+				}
+			
+			
+		};
+		
+		function accept(){
+			
+			var respbtn = document.querySelectorAll('.respbtn');
+			
+			var fuid = '';
+			var statuss = '';
+			
+			for(var i=0; i< respbtn.length; i++) {
+				   respbtn[i].addEventListener("click", bindClick(i));
+			};
+			
+			 function bindClick(i) {
+				return function(){
+						 console.log("you clicked button number " + i);
+						
+						fuid = $(this).data('frienduserid');
+						statuss = $(this).data('status');
+						
+						console.log('ajaja', statuss);
+						console.log('fuiid', fuid);
+						
+						$.ajax({
+							url:"server/frequestrespond.php",
+							type:"POST",
+							dataType:"JSON",
+							data:{
+								resp:statuss,
+								fuid:fuid	
+							},
+							success:function(frequestrespond){
+						
+							},
+							error: function(jqXHR, textStatus, errorThrown) {
+									console.log('frequestrespond error');
+									console.log(jqXHR.statusText, errorThrown, textStatus);
+							  
+							}
+							
+						});	
+				};
+			 };
+			
+			
+			
+		};
+			/*
+			for(var i = 0; i < respbtn.length; i++){
+				
+					respbtn[i].addEventListener('click', frequestrespond(i));
+					fuid = $(this).data('frienduserid');
+						statuss = $(this).data('status');
+						
+					
+			};
+			
+			function frequestrespond(){
+				
+				
+				
+		
+			
+			
+			
+					console.log('ajaja', statuss);
+					console.log('fuiid', fuid);
+			
+					
+					
+					
+					
+			
+			};
+			
+			
+		};
+	
+		
+		*/
+		
+		
+		function objectSize(the_object) {
+		  /* function to validate the existence of each key in the object to get the number of valid keys. */
+		  var object_size = 0;
+		  for (key in the_object){
+			if (the_object.hasOwnProperty(key)) {
+			  object_size++;
+			}
+		  }
+		  return object_size;
+		}
+		
+	
 			});	
 
 });
