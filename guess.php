0854634837987de4f416acabf13e0d33ecfc99d4
yingyi1204 <doraemonlyy@gmail.com>
2015-11-26 01:30:36 -0800

    working on guess page

--- guess.php
+++ guess.php
@@ -5,21 +5,21 @@
 
 
 
+    <div class = 'content'>
+            <h2>Take a Guess!</h2>
+            
+            <div class = 'guesscase'>
+
+            
+            </div>
+            
+            
+            
+        
+        </div>
+
                 <h1> Click On Your Guess</h1>
 
-
-         <div id = "guess">
-        <a href="wrong.php"><img id="first"  src="img/characters/character6.png"></a>
-         <a href="youwon.php"><img src="img/characters/character7.png"></a>
-          <a href="wrong.php"><img src="img/characters/character8.png"></a>
-       	 <a href="wrong.php"><img id="first" src="img/characters/character5.png"></a>
-      	 <a href="wrong.php"><img src="img/characters/character9.png"></a>
-      	  <a href="youwon.php"><img src="img/characters/character10.png"></a>
-       	 <a href="wrong.php"><img id="first" src="img/characters/character3.png"></a>
-		 <a href="wrong.php"><img src="img/characters/character11.png"></a>
-       	 <a href="wrong.php"><img id="last" src="img/characters/character4.png"> </a>
-       
-    </div>
 
 
                 <div class='buttonDiv'>
@@ -29,9 +29,108 @@
                  <a href='game.php' class="btn btn-blue" > Cancel</a>
                      
                 </div>
+                
+                
+       <script src='js/backbtn.js'></script>
 
 
-        <script src='js/backbtn.js'></script>
+<script>
+    
+    
+	  console.log('hi');
+	  
+	  
+	  
+	  $.ajax({
+			url:"server/guesswho-server.php",
+			type:"POST",
+			dataType:"JSON",
+			data:{
+				guesswho: 'showcharacters'
+				},
+			success:function(guesswhoresp){
+				
+				console.log("guesswhoresp is:", guesswhoresp);	
+				
+				var guesswholength = objectSize(guesswhoresp);
+                
+                console.log('guesswholength', guesswholength);
+                
+				var guesscase = document.querySelector('.guesscase');
+                var charname = document.createElement('h3');
+        
+				
+				for(var i=0; i< guesswholength; i++){
+                    
+                    var chara = document.createElement('img');
+                    
+                    console.log('chara:', guesswhoresp[i].character_img);
+                    chara.src = guesswhoresp[i].character_img;
+                    charname.innerHTML = guesswhoresp[i].character_name;
+
+                    
+                    guesscase.appendChild(chara);
+                    guesscase.appendChild(charname);
+                    
+                    
+                    
+                };
+				
+				
+				
+				
+			},
+			 error: function(jqXHR, textStatus, errorThrown) {
+                        //console.log(jqXHR.statusText, textStatus, errorThrown);
+                        console.log("guesswho error", jqXHR.statusText, textStatus);
+                  
+			
+		
+			}
+			
+		});	
+		
+		function objectSize(the_object) {
+		  /* function to validate the existence of each key in the object to get the number of valid keys. */
+		  var object_size = 0;
+		  for (key in the_object){
+			if (the_object.hasOwnProperty(key)) {
+			  object_size++;
+			}
+		  }
+		  return object_size;
+		}
+  
+  /*
+   var themes = ["cruising", "hoho", "lightscamera",  "onceuponamurder"];
+                var themeImg = ["img/themes/cruising.png", "img/themes/hoho.png",  "img/themes/lightscamera.png", "img/themes/onceuponamurder.png"];
+                
+                var themecase = document.querySelector('.themecase');
+               
+                for(var i=0; i<themes.length; i++){
+                    
+                    var imge = document.createElement('img');
+                    
+                    
+                    imge.src = themeImg[i];
+                    
+                    //themecase.innerHTML = '<a href = "game.html"><img src ="' + themeImg[i] + '">';
+                    
+                    themecase.appendChild(imge);
+                    
+                    
+                    
+                    
+                };
+            
+*/
+  
+	
+
+    
+</script>
+
+
     
 <?php
 	include("footer.php");
---
+++ server/guesswho-server.php
@@ -0,0 +1,86 @@
+<?php
+    // http://php.net/manual/en/function.session-start.php
+    // http://stackoverflow.com/questions/11768816/php-session-variables-not-preserved-with-ajax
+    // http://stackoverflow.com/questions/9560240/how-session-start-function-works
+    // get the session
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+	$DBHost = 'localhost';
+	$dblogin = 'root';
+	$DBpassword = 'root';
+	$DBname = 'mystery';
+
+
+
+
+    $methodType = $_SERVER['REQUEST_METHOD'];
+    $data = array("status" => "fail", "msg" => "$methodType");
+
+    if ($methodType === 'POST') {
+
+
+        if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+ 
+            if(isset($_POST["guesswho"]) && !empty($_POST["guesswho"])){
+
+                   // $guesswho = $_POST["guesswho"];
+                // get the data from the post and store in variables
+               
+                try {
+                    $conn = new PDO("mysql:host=$DBHost;dbname=$DBname", $dblogin, $DBpassword);
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    //$sql = "SELECT * FROM characters";
+	
+
+                    $statement = $conn->prepare("SELECT * FROM characters");
+        
+					//$statement->execute(array(":guesswhoinfo" => $guesswhoinfo));
+                    $statement->execute();
+
+                    // this should be one if there's a user by that user value and password value
+                    $count = $statement->rowCount();
+				
+                   //if($count > 0) {
+						
+						$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+						
+						//$guesswhoinfo = array();
+					
+                        //$data = $rows;
+					//$data = array($rows);
+					  $data = array($rows[0]);
+                       // $data = array("status" => "success", "characters" => $guesswhoinfo);
+
+
+
+
+
+                } catch(PDOException $e) {
+                    $data = array("status" => "fail", "msg" => $e->getMessage());
+                }
+
+
+            } else {
+                $data = array("status" => "fail", "msg" => "Either login or password were absent.");
+            }
+
+
+
+        } else {
+            // not AJAX
+            $data = array("status" => "fail", "msg" => "Has to be an AJAX call.");
+        }
+
+
+    } else {
+        // simple error message, only taking POST requests
+        $data = array("status" => "fail", "msg" => "Error: only POST allowed.");
+    }
+
+    echo json_encode($data, JSON_FORCE_OBJECT);
+
+?>
