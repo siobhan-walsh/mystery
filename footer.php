2ea82ad323d86346f0f36b1c57441718f612987f
JessD <jess423s@gmail.com>
2015-12-02 17:20:47 -0800

    styling fonts and css

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
         <div id = 'footer' class = 'footer'>
            <!--  <a href='themes.php'><img id= 'homey' src = 'img/homeicon.png' alt='home'></a> -->
             <a href = 'direction.php'><img src = 'img/gameicon.png' alt='game'></a>
-            <a href='profile.php'><img id='profiley' src = 'img/profileicon.png' alt='profile'></a>
+            
            
             <a href= 'settings.php'><img src = 'img/settingsicon.png' alt='settings'></a>
         
