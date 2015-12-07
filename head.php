f10bbcddb3d014dd6333bc5667c100f06056a7b9
siobhan-walsh <siobhanemmarose@gmail.com>
2015-12-06 16:08:15 -0800

    adding a favicon

--- head.php
+++ head.php
@@ -8,6 +8,7 @@
        <link href='https://fonts.googleapis.com/css?family=Merriweather+Sans:300,700' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="css/mediaqueries.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="icon" type="image/png" href="img/favicon.png" />
        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
